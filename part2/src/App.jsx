47c56775d7acd864ce0834f3ca2c3c995112a025
JorgeBencomo08 <bencomonavasjr@email.com>
2026-05-14 22:49:15 -0300

    ejercicios 2.1 2.2 y 2.3 hechos

---
+++ part2/src/App.jsx
@@ -0,0 +1,40 @@
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+const Course = ({course}) =>{
+  return (
+    <div>
+   <h1>{course.name}</h1>
+    {course.parts.map(part => (
+        <p key={part.id}>{part.name} {part.exercises}</p>
+      ))}
+ <p>Total of exercises = {course.parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
+    </div>
+  )
+
+}
+
+export default App
