2f1867b1413a098803fc795190d7c37c41ef1b3d
JorgeBencomo08 <bencomonavasjr@email.com>
2026-05-21 20:25:14 -0300

    ejercicios 2.9 y 2.10 hechos

--- part2/formularios/src/App.jsx
+++ part2/formularios/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -13,22 +17,28 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
       return
     }
-    setPersons(persons.concat({ name: newName, number: newNumber }))
+    setPersons(persons.concat({ name: newName, number: newNumber, id: persons.length + 1 }))
     setNewName('')
     setNewNumber('')
   }
 
+  const personsToShow = filter
+    ? persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()))
+    : persons
+
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>name: <input value={newName} onChange={(e) => setNewName(e.target.value)} /></div>
         <div>number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} /></div>
         <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, i) => (
-        <p key={i}>{person.name} {person.number}</p>
+      {personsToShow.map(person => (
+        <p key={person.id}>{person.name} {person.number}</p>
       ))}
     </div>
   )
